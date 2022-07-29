535c2f1e0f73b7c779c848bc55695bad92be2810
Gagandeep Singh65 <gagandeepswaraj65@gmail.com>
2022-07-30 04:22:31 +0530

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+/*eslint-disable*/
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    // this switch can be assumed to be similar to JS switch
+    // as when the route path is exact it is rendered otherwise
+    // the default route is rendered
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+      <Route>This is 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
